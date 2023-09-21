9b581f5d1b6a8b40d895367fe8c25bc610e3e2b1
nihad <nihadbaba.2800@gmail.com>
2023-09-20 22:01:51 -0400

    I'm cool.

---
+++ src/day10_if_statements/OverTime.java
@@ -0,0 +1,30 @@
+//package day10_if_statements;
+//
+//public class OverTime {
+//            /*
+//
+//        create a double variable for your hourly rate
+//        create a double variable for the number of hours worked
+//
+//        calculate your net pay, with consideration of overtime pay
+//
+//            if you worked more than 40 hours you get overtime pay for the hours past 40. Overtime pay is 1.5 the normal rate
+//                (if you worked 43 hours, you will get 40 hours normal pay and 3 hours overtime pay)
+//     */
+//    public static void main(String[]  args) {
+//        double hourlyRate = 60;
+//        double numberOfHours = 42;
+//        double netPay = hourlyRate * numberOfHours
+//        double overTimeHours;
+//        /*
+//        if numOfHours > 40
+//        overTimeHours = numberOfHours - 40;
+//         netPay = (hourlyRate * 40) + (overTimeHours * hourlyRate * 1.5)
+//         message = You worked 42 hours at a rate of $60, but 2 of the hours were overtime
+//         */
+//        if ( numberOfHours > 40) {
+//            netPay = hourlyRate * 40 + overTimeHours * hourlyRate * 1.5;
+////            message = "You worked  " + numberOfHours
+//        }
+//    }
+//}
---
+++ src/day10_if_statements/ifWithoutBrackets.java
@@ -0,0 +1,9 @@
+package day10_if_statements;
+
+public class ifWithoutBrackets {
+    public static void main(String[] args) {
+        if(4 > 2){
+            System.out.println("Hello");
+        }
+    }
+}
