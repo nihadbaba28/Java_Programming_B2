9cfbf69209a78e3bd58759450223b15b70dbdea8
nihad <nihadbaba.2800@gmail.com>
2023-10-07 12:07:43 -0400

    More String Methods

---
+++ src/day18_loops/SwitchInLoop.java
@@ -0,0 +1,31 @@
+package day18_loops;
+
+public class SwitchInLoop {
+    public static void main(String[] args) {
+      int i =0;
+        for ( i = 0; i <= 5 ; i++) {
+            switch (i){
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+            }
+
+        }
+        System.out.println(i);
+    }
+}
